2f994f009991648b1612e3028c4af6e45071be63
starlight258 <audwl03071@gmail.com>
2022-02-23 19:45:32 +0900

    wss

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+//프론트엔드(Front-end)
+const frontSocket = new WebSocket(`ws://${window.location.host}`);
+//소켓: 서버로의 연결
+frontSocket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+frontSocket.addEventListener("message", (message) => {
+  console.log("New message:", message.data);
+});
+
+frontSocket.addEventListener("close", () => {
+  console.log("Disconnected to Server ❌");
+});
+
+setTimeout(() => {
+  frontSocket.send("hello from the browser!");
+}, 10000); //10초 뒤에 메세지 보내기
--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
+//백엔드(Back-end)
 import express from "express";
-
+import http from "http";
+import { WebSocketServer } from "ws";
 const app = express();
 
 app.set("view engine", "pug"); //pug 사용
 app.set("views", __dirname + "/views");
+
 app.use("/public", express.static(__dirname + "/public")); //public 연결하기
 app.get("/", (req, res) => res.render("home")); //렌더링
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server }); //http서버 위에 ws서버 만들기
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => {
+    console.log("Disconnected from the Browser ❌");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!!!");
+}); //연결시 함수 실행
+server.listen(3000, handleListen);
